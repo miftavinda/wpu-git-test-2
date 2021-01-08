af04e8b364c25afba49bad0fabd82e942e75bd4d
Miftavinda <miftavinda@gmail.com>
2021-01-08 17:34:04 +0700

    menambahkan paragraf ke index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
     <h1>hello </h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores hic sit architecto in quia vero obcaecati mollitia, vel, nulla perferendis temporibus blanditiis sint ratione libero culpa unde aliquam velit! Sit?
     </p>
+    <ul>
+    <li>kalo</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea ullam laborum tempore, obcaecati ratione ab reiciendis quidem! Dicta laudantium fugit id! Neque similique est odio enim molestiae dicta quaerat officia.
+    </li>
+    </ul>
 </body>
 </html>
